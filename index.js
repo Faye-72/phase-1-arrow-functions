2bdf4e94d1510cf7acf8d8b136d428a332ee5014
faith mitei <faithmitey@gmail.com>
2022-12-07 23:04:41 +0300

    Complete the assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = function(){
+   return  (2000/100)
+}
+divide()
+const square = (x)=>{
+    // let x=2
+    return (x*x)
+}
+square(2)
+
+const add =(a,b)=>{
+    // let a = 3
+    // let b = 4
+    return (a+b) 
+}
+add(3,4)
